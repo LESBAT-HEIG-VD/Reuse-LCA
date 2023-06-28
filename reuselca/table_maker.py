a740025562eda9092928ceb1f00513014855ac74
mijafro <mija.frossard@protonmail.com>
2023-06-28 20:44:28 +0200

    html Faraday DataTable

--- reuselca/table_maker.py
+++ reuselca/table_maker.py
@@ -20,98 +20,183 @@ if __name__ == "__main__":
 
     cols = ["Map_id", "Element", "Category", "Reuse_origin", "Age",
             "Reference_service_lifetime", "Service_life_extension",
-            "Element_area", "Volume", "Mass", "Mass with loss", "Loss",
+            "Element_area", "Volume", "Mass", "Mass with loss", "Loss", "Loss data rating",
             "DIS - Operating time", "DIS - Operating unit", "DIS - Machine power",
             "DIS - Energy type", "DIS - Default dismantling", "GWP_DIS",
-            "TR_A2 - Total distance", "TR_A2 - Data rating", "TR_A2 - Vehicle", "GWP_TR_A2",
-            "ST - Storage place", "ST - Storage space", "ST - Unit of storage",
+            "TR_A2 - Total distance", "TR_A2 - Data rating distance", "TR_A2 - Vehicle", "TR_A2 - Data rating vehicle", "GWP_TR_A2",
+            "ST - Storage place", "ST - Data rating storage place", "ST - Storage space", "ST - Unit of storage",
             "ST - Default space", "ST - Duration (years)", "ST - Default duration", "GWP_ST",
-            "MOD - Data type", "MOD - LCA dataset name", "MOD - Ratio impact", "MOD - Operating time (hours)",
-            "MOD - Operating unit", "MOD - Machine power", "MOD - Energy type", "GWP_MOD",
-            "KBOB_dataset_name", "KBOB_dataset_id", "GWP_LOSSES",
-            "TR_A4 - Total distance", "TR_A4 - Data rating", "TR_A4 - Vehicle", "GWP_TR_A4",
+            "MOD - Data type", "MOD - LCA dataset name", "MOD - Ratio impact", "MOD - Operating time",
+            "MOD - Operating unit", "MOD - Machine power", "MOD - Energy type", "MOD - Data rating", "GWP_MOD",
+            "KBOB_group", "KBOB_dataset_name", "KBOB_dataset_id", "Function_quality", "Function_quality_data_rating",
+            "GWP_LOSSES", "TR_A4 - Total distance", "TR_A4 - Data rating distance", "TR_A4 - Vehicle", "TR_A4 - Data rating vehicle", "GWP_TR_A4",
             "GWP_A1-A5", "GWP_New_A1-A5", "GWP_Avoided_prod", "GWP_Avoided_Waste", "GWP_Avoided_Total"]
 
     full_table = reused_data[cols]
     # convert to int
-    for col in ["Map_id", "Age", "Reference_service_lifetime", "Service_life_extension",
+    int_cols = ["Map_id", "Age", "Reference_service_lifetime", "Service_life_extension",
                 "DIS - Machine power", "TR_A2 - Total distance"
-                ]:
-        full_table[col] = full_table[col].fillna(0)
-        full_table[col] = full_table[col].astype(int)
+                ]
+    full_table[int_cols] = full_table[int_cols].fillna(0)
+    full_table[int_cols] = full_table[int_cols].astype(int)
+
+    float_cols = ["Element_area", "Volume", "Mass", "Mass with loss", "Loss",
+                "DIS - Operating time", "GWP_DIS", "GWP_TR_A2", "ST - Storage space",
+                "ST - Duration (years)", "GWP_ST", "MOD - Ratio impact",
+                "MOD - Operating time", "GWP_MOD", "GWP_LOSSES","GWP_TR_A4",
+                "GWP_A1-A5", "GWP_New_A1-A5", "GWP_Avoided_prod", "GWP_Avoided_Waste", "GWP_Avoided_Total"
+                ]
+    full_table[float_cols] = full_table[float_cols].fillna(0)
+    # full_table[float_cols] = full_table[float_cols].astype(float)
+    full_table[float_cols] = full_table[float_cols].apply(pd.to_numeric, errors='coerce')
+
+    def format_float(num):
+        if isinstance(num, float):
+            if num == 0:
+                return '{:.0f}'.format(num)
+            if -0.01 < num < 0.01:
+                return '{:.4f}'.format(num)  # Two digits after decimal for numbers between -1 and 1
+            if -1 < num < 1:
+                return '{:.3f}'.format(num)  # Two digits after decimal for numbers between -1 and 1
+            if -10< num < 10:
+                return '{:.2f}'.format(num)  # Two digits after decimal for numbers between -1 and 1
+            if -100 < num < 100:
+                return '{:.1f}'.format(num)  # Two digits after decimal for numbers between -1 and 1
+            else:
+                return '{:.0f}'.format(num)
+        else:
+            return num
 
     def return_percentage(value):
-        return '{:.0%}'.format(value)
+        if isinstance(value,str):
+            return '{:.0%}'.format(float(value))
+        else:
+            return '{:.0%}'.format(value)
 
     full_table["Loss"] = full_table["Loss"].apply(return_percentage)
 
     # dismantling
+    def create_losses_col(row):
+        los_rating = row["Loss data rating"]
+        return get_rating_dot(los_rating)+row["Loss"]
+
     def create_dis_col(row):
         dis_rating = row["DIS - Default dismantling"]
-        if dis_rating == "Default (no data)":
+        if pd.isna(dis_rating):
+            return get_rating_dot("Not modelled (no data)")+"No information"
+        elif dis_rating == "Default (no data)":
             return get_rating_dot(dis_rating)+'UVEK DQRv2 2021: deconstruction, average/CH U'
-        if dis_rating == "Estimate":
+        elif dis_rating == "Not concerned":
+            if "surplus" in row["Reuse_origin"]:
+                return get_rating_dot(dis_rating)+"Surplus, no dismantling"
+            else:
+                return get_rating_dot(dis_rating)+"No dismantling"
+        else:
             return get_rating_dot(dis_rating)+'{} W, {} (CH), {} {} '.format(row["DIS - Machine power"],row["DIS - Energy type"],
                                              row["DIS - Operating time"],row["DIS - Operating unit"])
-        if row["DIS - Default dismantling"] == "Not concerned":
-            return get_rating_dot(dis_rating)+' -'
 
     def create_st_col(row):
+        place_rating = row["ST - Data rating storage place"]
         duration_rating = row["ST - Default duration"]
         space_rating = row["ST - Default space"]
-        if pd.isna(row["ST - Storage place"]):
-            return "No storage"
-        if pd.isna(duration_rating):
-            return "No storage"
-        elif duration_rating == "Default (no data)":
-            duration = get_rating_dot(duration_rating)+'{:.2f} years'.format(Building.hypotheses["Default storage duration (year)"].values[0])
-        elif duration_rating == "Estimate":
-            duration = get_rating_dot(duration_rating)+'{:.2f} years'.format(row["ST - Duration (years)"])
-        if pd.isna(space_rating):
-            return "No storage"
-        elif space_rating == "Default (no data)":
+
+        if place_rating == "Not concerned" or duration_rating == "Not concerned" or space_rating == "Not concerned":
+            return get_rating_dot("Not concerned")+"No storage"
+        elif pd.isna(row["ST - Storage place"]) or row["ST - Storage place"]=='nan':
+            return get_rating_dot("Not modelled (no data)")+"No information"
+        else:
+            place = get_rating_dot(place_rating)+'{}'.format(row["ST - Storage place"])
+
+        if duration_rating == "Default (no data)":
+            duration = get_rating_dot(duration_rating)+'{:.2f} a'.format(Building.hypotheses["Default storage duration (year)"].values[0])
+        else:
+            duration = get_rating_dot(duration_rating)+'{:.2f} a'.format(row["ST - Duration (years)"])
+
+        if space_rating == "Default (no data)":
             if row["ST - Unit of storage"] == "m2":
                 space = get_rating_dot(space_rating)+'{:.3f} m2'.format(row["Element_area"])
             elif row["ST - Unit of storage"] == "m3":
                 space = get_rating_dot(space_rating)+'{:.3f} m3'.format(row["Volume"])
-        elif space_rating == "Estimate":
-            if row["ST - Unit of storage"] == "m2":
-                space = get_rating_dot(space_rating)+'{:.3f} m2'.format(row["ST - Storage space"])
-            elif row["ST - Unit of storage"] == "m3":
-                space = get_rating_dot(space_rating)+'{:.3f} m3'.format(row["ST - Storage space"])
-        return duration+', '+space+' in {}'.format(row["ST - Storage place"])
+        else:
+            space = get_rating_dot(space_rating)+'{:.3f}'.format(row["ST - Storage space"])+' '+row["ST - Unit of storage"]
+        return duration+', '+space+', '+place
 
     def create_mod_col(row):
-        if pd.isna(row["MOD - Data type"]):
-            return "-"
-        elif row["MOD - Data type"] == "Reuse-LCA Modifications":
-            return "Reuse-LCA dataset: {} ({:.2f})".format(row["MOD - LCA dataset name"], row["MOD - Ratio impact"])
-        elif row["MOD - Data type"] == "Energy only":
-            return "{} W, {} (CH), {} {} ".format(row["MOD - Machine power"],row["MOD - Energy type"],
-                                             row["MOD - Operating time (hours)"],row["MOD - Operating unit"])
+        mod_rating = row["MOD - Data rating"]
+        if mod_rating == "Not concerned":
+            return get_rating_dot(mod_rating)+'No modification'
+        elif mod_rating == "Not modelled (no data)":
+            return get_rating_dot(mod_rating)+row["MOD - LCA dataset name"]
+        else:
+            if pd.isna(row["MOD - Data type"]) or row["MOD - Data type"]=='nan':
+                return get_rating_dot("Not modelled (no data)") + "No information"
+            elif row["MOD - Data type"] == "Reuse-LCA Modifications":
+                return get_rating_dot(mod_rating)+"Reuse-LCA: {}".format(row["MOD - LCA dataset name"])
+            elif row["MOD - Data type"] == "Energy only":
+                return get_rating_dot(mod_rating)+"{} W, {} (CH), {} {} ".format(row["MOD - Machine power"],row["MOD - Energy type"],
+                                                 row["MOD - Operating time (hours)"],row["MOD - Operating unit"])
+
     def create_tr_a2_col(row):
-        tr_rating = row["TR_A2 - Data rating"]
-        return get_rating_dot(tr_rating)+"{:.0f} km, {}".format(row["TR_A2 - Total distance"], row["TR_A2 - Vehicle"])
+        dist_rating = row["TR_A2 - Data rating distance"]
+        veh_rating = row["TR_A2 - Data rating vehicle"]
+        if pd.isna(dist_rating) or dist_rating == 'nan':
+            return get_rating_dot("Not modelled (no data)") + "No information"
+        if dist_rating == "Not concerned" and veh_rating == "Not concerned":
+            return get_rating_dot("Not concerned")+"No intermediate transport"
+        elif dist_rating == "Not concerned" or veh_rating == "Not concerned":
+            return get_rating_dot("Not modelled (no data)") + "No complete information"
+        else:
+            return get_rating_dot(dist_rating)+'{:.0f} km, '.format(row["TR_A2 - Total distance"])+get_rating_dot(veh_rating)+row["TR_A2 - Vehicle"]
+
     def create_tr_a4_col(row):
-        tr_rating = row["TR_A4 - Data rating"]
-        return get_rating_dot(tr_rating)+"{:.0f} km, {}".format(row["TR_A4 - Total distance"], row["TR_A4 - Vehicle"])
+        dist_rating = row["TR_A4 - Data rating distance"]
+        veh_rating = row["TR_A4 - Data rating vehicle"]
+        if pd.isna(dist_rating) or dist_rating == 'nan':
+            return get_rating_dot("Not modelled (no data)") + "No information"
+        if dist_rating == "Not concerned" and veh_rating == "Not concerned":
+            return get_rating_dot("Not concerned")+"No intermediate transport"
+        elif dist_rating == "Not concerned" or veh_rating == "Not concerned":
+            return get_rating_dot("Not modelled (no data)") + "No complete information"
+        else:
+            return get_rating_dot(dist_rating)+'{:.0f} km, '.format(row["TR_A4 - Total distance"])+get_rating_dot(veh_rating)+row["TR_A4 - Vehicle"]
 
-    full_table["Dismantling inventory"] = full_table.apply(lambda row: create_dis_col(row), axis=1)
-    full_table["Storage inventory"] = full_table.apply(lambda row: create_st_col(row), axis=1)
-    full_table["Modification inventory"] = full_table.apply(lambda row: create_mod_col(row), axis=1)
-    full_table["Transport A2 inventory"] = full_table.apply(lambda row: create_tr_a2_col(row), axis=1)
-    full_table["Transport A4 inventory"] = full_table.apply(lambda row: create_tr_a4_col(row), axis=1)
+    def create_neweq_col(row):
+        function_rating = row["Function_quality_data_rating"]
+        if "Reuse-LCA" in row["KBOB_group"]:
+            prefix = "Reuse-LCA: "
+        else:
+            prefix = "KBOB: "
+        if function_rating == "Default (no data)":
+            percent = "(" + get_rating_dot(function_rating) + "100%)"
+        else:
+            percent = "("+get_rating_dot(function_rating)+return_percentage(row["Function_quality"])+")"
+        return prefix+row["KBOB_dataset_name"]+" "+percent
 
-    cols2 = ["Map_id", "Element", "Category", "Reuse_origin", "Age",
-             "Element_area", "Volume", "Mass", "Mass with loss", "Loss",
-             "Service_life_extension",
-             "Dismantling inventory", "DIS - Default dismantling", "GWP_DIS",
-             "Transport A2 inventory", "TR_A2 - Data rating", "GWP_TR_A2",
-             "Storage inventory", "GWP_ST",
-             "Modification inventory", "GWP_MOD",
-             "Transport A4 inventory", "TR_A4 - Data rating", "GWP_TR_A4",
-             "KBOB_dataset_name", "KBOB_dataset_id","GWP_LOSSES",
-             "GWP_A1-A5", "GWP_New_A1-A5", "GWP_Avoided_prod", "GWP_Avoided_Waste", "GWP_Avoided_Total"]
+    def create_lifespan_ext(row):
+        return get_rating_dot("Hypothesis")+"{}".format(row["Service_life_extension"])
+
+    def create_reduction_col(row):
+        value = (row["GWP_A1-A5"]-row["GWP_New_A1-A5"])/row["GWP_New_A1-A5"]
+        return return_percentage(format_float(value))
+
+    full_table["Losses"] = full_table.apply(lambda row: create_losses_col(row), axis=1)
+    full_table["Dismantling"] = full_table.apply(lambda row: create_dis_col(row), axis=1)
+    full_table["Storage"] = full_table.apply(lambda row: create_st_col(row), axis=1)
+    full_table["Modification"] = full_table.apply(lambda row: create_mod_col(row), axis=1)
+    full_table["Transport A2"] = full_table.apply(lambda row: create_tr_a2_col(row), axis=1)
+    full_table["Transport A4"] = full_table.apply(lambda row: create_tr_a4_col(row), axis=1)
+    full_table["Conventional product and end-of-life"] = full_table.apply(lambda row: create_neweq_col(row), axis=1)
+    full_table["Service lifespan extension"] = full_table.apply(lambda row: create_lifespan_ext(row), axis=1)
+    full_table["Difference of production's GWP (%)"] = full_table.apply(lambda row: create_reduction_col(row), axis=1)
+
+    cols2 = ["Map_id", "Element", "Category",
+             "Reuse_origin", "Age", "Service lifespan extension",
+             "Element_area", "Volume", "Mass", "Mass with loss",
+             "Conventional product and end-of-life", "KBOB_dataset_id",
+             "Losses","Dismantling","Storage","Modification",
+             "Transport A2","Transport A4",
+             "GWP_LOSSES","GWP_DIS","GWP_ST","GWP_MOD","GWP_TR_A2","GWP_TR_A4",
+             "GWP_A1-A5", "GWP_New_A1-A5", "GWP_Avoided_prod", "GWP_Avoided_Waste", "GWP_Avoided_Total", "Difference of production's GWP (%)"]
 
     filtered_table = full_table[cols2]
 
@@ -124,32 +209,34 @@ if __name__ == "__main__":
                      "Volume":"Volume (m3)",
                      "Mass":"Mass used (kg)",
                      "Mass with loss":"Mass before losses (kg)",
-                     "Loss":"Losses",
-                     "Service_life_extension":"Extension of service lifespan",
-                     "Dismantling inventory":"Dismantling LCI",
-                     "DIS - Default dismantling":"Dismantling data rating",
-                     "GWP_DIS":"Dismantling GWP (kg CO2eq.)",
-                     "Transport A2 inventory":"Transport A2 LCI",
+                     "Losses":"Losses",
+                     "Service_life_extension":"Service lifespan extension",
+                     "Dismantling inventory":"Dismantling",
+                     "GWP_DIS":"GWP: Dismantling",
+                     "Transport A2 inventory":"Transport A2",
                      "TR_A2 - Data rating": "Transport A2 data rating",
-                     "GWP_TR_A2":"Transport A2 GWP (kg CO2eq.)",
-                     "Storage inventory":"Storage LCI",
-                     "GWP_ST":"Storage GWP (kg CO2eq.)",
-                     "Modification inventory":"Modification LCI",
-                     "GWP_MOD":"Modification GWP (kg CO2eq.)",
-                     "Transport A4 inventory":"Transport A2 GWP (kg CO2eq.)",
+                     "GWP_TR_A2":"GWP: Transport A2",
+                     "Storage inventory":"Storage",
+                     "GWP_ST":"GWP: Storage",
+                     "Modification inventory":"Modification",
+                     "GWP_MOD":"GWP: Modification",
+                     "Transport A4 inventory":"Transport A4",
                      "TR_A4 - Data rating":"Transport A4 data rating",
-                     "GWP_TR_A4":"Transport A4 GWP (kg CO2eq.)",
+                     "GWP_TR_A4":"GWP: Transport A4",
                      "KBOB_dataset_name":"KBOB dataset (for new prod. and EoL)",
                      "KBOB_dataset_id": "KBOB dataset id",
-                     "GWP_LOSSES":"Losses GWP (kg CO2eq.)",
-                     "GWP_A1-A5":"Project A1-A4 GWP (kg CO2eq.)",
-                     "GWP_New_A1-A5":"New prod. eq. A1-A4 GWP (kg CO2eq.)",
-                     "GWP_Avoided_prod":"Avoided production GWP (kg CO2eq.)",
-                    "GWP_Avoided_Waste":"Avoided waste treatment GWP (kg CO2eq.)",
-                    "GWP_Avoided_Total":"Total avoided GWP (kg CO2eq.)"
+                     "GWP_LOSSES":"GWP: Losses",
+                     "GWP_A1-A5":"GWP: Total A1-A4",
+                     "GWP_New_A1-A5":"GWP: Conventional equivalent A1-A4",
+                     "GWP_Avoided_prod":"GWP: Avoided, production",
+                    "GWP_Avoided_Waste":"GWP: Avoided, waste treatment",
+                    "GWP_Avoided_Total":"GWP: Avoided, total",
+                     "Difference of production's GWP (%)":"GWP: difference in production (%)"
                      }
 
     final_table = filtered_table.rename(columns=mapping_names)
+    # # Apply formatting to DataFrame
+    final_table = final_table.applymap(format_float)
     html = final_table.to_html(index=False)
     html = html.replace("&amp;","&")
     html = html.replace("&lt;","<")
--- reuselca/utils_html.py
+++ reuselca/utils_html.py
@@ -5,6 +5,8 @@ def get_rating_dot(rating):
         return '<span style="color: #57e32c;" class="data-rating-dot">&bull;</span>'
     elif rating == "Estimate":
         return '<span style="color: #b7dd29;" class="data-rating-dot">&bull;</b></span>'
+    elif rating == "Hypothesis":
+        return '<span style="color: #ffe234;" class="data-rating-dot">&bull;</b></span>'
     elif rating == "Default (no data)":
         return '<span style="color: #ffa534;" class="data-rating-dot">&bull;</b></span>'
     elif rating == "Not modelled (no data)":
